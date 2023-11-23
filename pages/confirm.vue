638de450907642a090a4773b9f00f89275847d06
oktapascal <oktaiscool@gmail.com>
2023-11-24 02:49:37 +0700

    create halaman konfirmasi email

---
+++ pages/confirm.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="h-screen flex justify-center items-center">
+    <div class="card w-[28rem]">
+      <div class="card-header text-center">
+        <i class="ri-mail-check-line text-6xl"></i>
+        <h2 class="card-title">Konfirmasi Email</h2>
+      </div>
+      <div class="card-content text-center">
+        Terima kasih telah mendaftar. Untuk melanjutkan proses, silahkan cek email anda untuk memverifikasi.
+      </div>
+    </div>
+  </div>
+</template>
