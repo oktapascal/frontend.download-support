60aa1c83b250ff75c512fcd0be5592503869672d
oktapascal <oktaiscool@gmail.com>
2023-11-23 14:09:15 +0700

    Create layout home page

---
+++ layouts/private.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="grid grid-cols-12 gap-1">
+    <div class="relative bg-white flex flex-col py-5 col-span-2 h-screen">
+      <button type="button"
+        class="btn py-3 px-2 justify-start rounded-none text-lg border-b hover:bg-neutral-200 active:bg-neutral-300">
+        <i class="ri-inbox-2-fill"></i>
+        <span class="pl-4 text-base">Semua</span>
+      </button>
+      <button type="button"
+        class="btn py-3 px-2 justify-start rounded-none text-lg border-b hover:bg-neutral-200 active:bg-neutral-300">
+        <i class="ri-mail-send-line"></i>
+        <span class="pl-4 text-base">Terkirim</span>
+      </button>
+      <button type="button"
+        class="btn py-3 px-2 justify-start rounded-none text-lg border-b hover:bg-neutral-200 active:bg-neutral-300">
+        <i class="ri-shape-2-line"></i>
+        <span class="pl-4 text-base">Diproses</span>
+      </button>
+      <button type="button"
+        class="btn py-3 px-2 justify-start rounded-none text-lg hover:bg-neutral-200 active:bg-neutral-300">
+        <i class="ri-verified-badge-line"></i>
+        <span class="pl-4 text-base">Selesai</span>
+      </button>
+      <hr class="border-[1.5px] border-neutral-200 my-4" />
+      <button type="button"
+        class="btn py-3 px-2 justify-start rounded-none text-lg hover:bg-neutral-200 active:bg-neutral-300">
+        <i class="ri-account-box-line"></i>
+        <span class="pl-4 text-base">Akun Saya</span>
+      </button>
+      <button type="button"
+        class="btn py-3 px-2 justify-start rounded-none text-lg hover:bg-neutral-200 active:bg-neutral-300">
+        <i class="ri-logout-circle-line"></i>
+        <span class="pl-4 text-base">Keluar</span>
+      </button>
+      <div class="px-2 absolute bottom-5">
+        <button type="button" class="btn w-48 h-10 text-base bg-blue-500 text-white hover:bg-blue-600 active:bg-blue-700">
+          Tambah Pengaduan
+        </button>
+      </div>
+    </div>
+    <slot />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'private'
+})
+</script>
+
+<template>
+  <div class="card bg-white rounded-none col-span-3">list data</div>
+  <div class="card bg-white rounded-none col-span-5">conversation</div>
+  <div class="card bg-white rounded-none col-span-2">history </div>
+</template>
