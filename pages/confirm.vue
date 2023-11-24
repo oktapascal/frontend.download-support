0c36671ef3b1846901ff7c36eb6d0b1df00e0182
oktapascal <oktaiscool@gmail.com>
2023-11-25 02:09:42 +0700

    update confirmation page

--- pages/confirm.vue
+++ pages/confirm.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="h-screen flex justify-center items-center">
-    <div class="card w-[28rem]">
+    <div class="card w-[29rem]">
       <div class="card-header text-center">
         <i class="ri-mail-check-line text-6xl"></i>
         <h2 class="card-title">Konfirmasi Email</h2>
       </div>
       <div class="card-content text-center">
-        Terima kasih telah mendaftar. Untuk melanjutkan proses, silahkan cek email anda untuk memverifikasi.
+        Terima kasih telah mendaftar. Untuk melanjutkan proses, silahkan cek email anda untuk melakukan verifikasi.
       </div>
     </div>
   </div>
